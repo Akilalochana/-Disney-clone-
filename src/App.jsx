7efb17121c458fc371e76cdd2c119b4e392617cc
Akila Lochana <168577881+Akilalochana@users.noreply.github.com>
2025-01-21 23:35:35 +0530

    add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home/Home'
+import Video from './Pages/Video/Video' 
 
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [sidebar,setSidebar] = useState(true);
 
   return (
     <div>
-      <Navbar/>
+      <Navbar setSidebar={setSidebar}/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home sidebar={sidebar}/>}/>
         <Route path='/video/:categoryId/:videoId' element={<Video/>}/> 
       </Routes>
     </div>
@@ -21,3 +23,4 @@ function App() {
 }
 
 export default App
+
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
-import Navbar from '../../Components/Navbar/Navbar'
+import Sidebar from '../../Components/Sidebar/Sidebar'
 
-const Home = () => {
+
+
+const Home = ({sidebar}) => {
   return (
     <>
-    <Navbar/>
+      <Sidebar sidebar={sidebar}/>
 
     </>
   )
